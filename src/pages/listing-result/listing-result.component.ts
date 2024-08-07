b43ca1bf7a0737b87618220cee73d8bb8e4db9f3
Grace Bulckens <grace.bulckens@gmail.com>
2024-08-07 11:40:17 +0200

    Creating listing-result page

--- src/pages/listing-result/listing-result.component.ts
+++ src/pages/listing-result/listing-result.component.ts
@@ -1,11 +1,25 @@
-import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { ChangeDetectionStrategy, Component, inject } from "@angular/core";
+import { ListingResultFacade } from "./store/listing-result.facade";
+import { PushPipe } from "@ngrx/component";
+import { MatTableModule } from "@angular/material/table";
+import { MatButtonModule } from "@angular/material/button";
+import { RouterLink } from "@angular/router";
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 @Component({
   selector: "app-listing-result",
   standalone: true,
-  imports: [],
+  imports: [PushPipe, MatTableModule, MatButtonModule, RouterLink, MatProgressSpinnerModule],
   templateUrl: "./listing-result.component.html",
   styleUrl: "./listing-result.component.scss",
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ListingResultComponent {}
+export class ListingResultComponent {
+  private facade = inject(ListingResultFacade);
+
+  listings$ = this.facade.listings$;
+
+  loading$ = this.facade.loading$;
+
+  displayedColumns: string[] = ["id", "title", "price", "link"];
+}
