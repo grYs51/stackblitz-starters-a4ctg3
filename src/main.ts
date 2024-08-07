0bc39afcaf8b0d3464d999a8a253d3775f08196e
Grace Bulckens <grace.bulckens@gmail.com>
2024-08-07 09:40:04 +0200

    Added ngrx

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,30 @@
-import { Component } from '@angular/core';
-import { bootstrapApplication } from '@angular/platform-browser';
-import 'zone.js';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideRouter, RouterOutlet } from '@angular/router';
-import { routes } from './app.routes';
+import { Component, isDevMode } from "@angular/core";
+import { bootstrapApplication } from "@angular/platform-browser";
+import "zone.js";
+import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
+import { provideRouter, RouterOutlet } from "@angular/router";
+import { routes } from "./app.routes";
+import { provideStore } from "@ngrx/store";
+import { provideStoreDevtools } from "@ngrx/store-devtools";
+import { provideEffects } from "@ngrx/effects";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
   imports: [RouterOutlet],
-  template: `
-    <router-outlet />
-  `,
+  template: `<router-outlet />`,
 })
 // eslint-disable-next-line @angular-eslint/component-class-suffix
 export class App {
-  name = 'Angular';
+  name = "Angular";
 }
 
 bootstrapApplication(App, {
-  providers: [provideAnimationsAsync(), provideRouter(routes)]
+  providers: [
+    provideAnimationsAsync(),
+    provideRouter(routes),
+    provideStore(),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
+    provideEffects(),
+  ],
 });
