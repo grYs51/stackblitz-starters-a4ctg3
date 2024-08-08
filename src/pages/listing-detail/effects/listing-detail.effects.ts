5fffd2b5737187cec81e0027c85a2b3d3a01ea91
Grace Bulckens <grace.bulckens@gmail.com>
2024-08-08 09:59:31 +0200

    Check if item exist before fetching

--- src/pages/listing-detail/effects/listing-detail.effects.ts
+++ src/pages/listing-detail/effects/listing-detail.effects.ts
@@ -1,7 +1,7 @@
 import { inject, Injectable } from "@angular/core";
 import { Actions, createEffect, ofType } from "@ngrx/effects";
 
-import { switchMap, map, catchError, of } from "rxjs";
+import { switchMap, map, catchError, of, withLatestFrom } from "rxjs";
 import { ApiService } from "../../../shared/services/api.service";
 import { ActivatedRoute, Router } from "@angular/router";
 import { navigatedTo } from "../../../shared/utils/router";
@@ -11,6 +11,7 @@ import {
   fetchListingDetailFailure,
   fetchListingDetailSuccess,
 } from "../store/listing-detail/listing-detail.actions";
+import { ListingResultFacade } from "../../../features/listing/store/listing-result.facade";
 @Injectable()
 export class ListingDetailEffects {
   actions$ = inject(Actions);
@@ -21,6 +22,8 @@ export class ListingDetailEffects {
 
   activatedRoute = inject(ActivatedRoute);
 
+  listingResultFacade = inject(ListingResultFacade);
+
   init$ = createEffect(() =>
     this.router.events.pipe(
       navigatedTo(ListingDetailComponent),
@@ -28,12 +31,23 @@ export class ListingDetailEffects {
     )
   );
 
+
+
   fetchListings$ = createEffect(() =>
     this.actions$.pipe(
       ofType(fetchListingDetail),
-      switchMap(({ id }) => this.api.fetchListing(id)),
-      map((listing) => fetchListingDetailSuccess({ listing })),
-      catchError((error) => of(fetchListingDetailFailure({ error })))
+      withLatestFrom(this.listingResultFacade.listings$),
+      switchMap(([{ id }, listings]) => {
+        const existingListing = listings.find((listing) => listing.id === id);
+        if (existingListing) {
+          return of(fetchListingDetailSuccess({ listing: existingListing }));
+        } else {
+          return this.api.fetchListing(id).pipe(
+            map((listing) => fetchListingDetailSuccess({ listing })),
+            catchError((error) => of(fetchListingDetailFailure({ error })))
+          );
+        }
+      })
     )
   );
 }
